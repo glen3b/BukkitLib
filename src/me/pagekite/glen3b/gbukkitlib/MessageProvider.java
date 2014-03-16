0c7c181b2e7f321731ad1b910645205dedf5d7ad
Glen Husman <wappokickingdeer@gmail.com>
2014-03-16 14:45:02 -0700

    Message provider interface added This is registered as a service, and it can provide message to the client application. It is, by default, registered to the plugin itself, which by default only provides internally used messages. The backend for the default implementation is the config file.
    *This commit also amends the previous commit, as some files were missed

---
+++ src/me/pagekite/glen3b/gbukkitlib/MessageProvider.java
@@ -0,0 +1,42 @@
+package me.pagekite.glen3b.gbukkitlib;
+
+import java.util.Set;
+
+/**
+ * A provider of user-friendly color-formatted messages.
+ * @author Glen
+ */
+public interface MessageProvider {
+
+	/**
+	 * Gets the set of provided messages. This is a collection of keys, not values.
+	 * @return A {@link java.util.Set} of keys of messages provided by this {@link me.pagekite.glen3b.gbukkitlib.MessageProvider}.
+	 */
+	public Set<String> getProvidedMessages();
+	
+	/**
+	 * Gets the message with the specified message ID.
+	 * @return The color-formatted message with the specified ID.
+	 */
+	public String getMessage(String messageId);
+	
+	/**
+	 * Adds a message to this provider.
+	 * @param key The identifying key of the message.
+	 * @param value The value of the message, which will have color codes preceded with the ampersand character translated appropriately.
+	 * @exception java.lang.IllegalStateException Thrown if this is a read-only message provider.
+	 */
+	public void setMessage(String key, String value) throws IllegalStateException;
+	
+	/**
+	 * Saves all messages stored by this provider to the backend, if applicable.
+	 * @exception java.lang.IllegalStateException Thrown if this is a read-only message provider.
+	 */
+	public void saveMessages();
+	
+	/**
+	 * Determines the read/write status of this message provider.
+	 * @return If this is a read-only message provider.
+	 */
+	public boolean isReadOnly();
+}
