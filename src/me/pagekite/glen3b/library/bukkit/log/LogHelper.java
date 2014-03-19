857c154437ae09783f95b614c2f5d37b696be43b
Glen Husman <wappokickingdeer@gmail.com>
2014-03-19 15:37:24 -0700

    LogHelper finished

--- src/me/pagekite/glen3b/library/bukkit/log/LogHelper.java
+++ src/me/pagekite/glen3b/library/bukkit/log/LogHelper.java
@@ -40,6 +40,60 @@ public class LogHelper {
 	}
 	
 	/**
+	 * Process the log statement, stripping color if need be.
+	 * @param msg The statement to process.
+	 * @param format The message formatting items.
+	 * @return The processed statement.
+	 */
+	protected String processLogStatement(String msg, Object... format){
+		if(format != null && format.length > 0){
+			msg = String.format(msg, format);
+		}
+		
+		if(isColorStrippingEnabled()){
+			msg = ChatColor.stripColor(msg);
+		}
+		
+		return msg;
+	}
+	
+	/**
+	 * Logs a severe error.
+	 * @param message The message to log.
+	 */
+	public void logError(String message){
+		logError(message, null);
+	}
+	
+	/**
+	 * Logs a severe error, including a stack trace if error is not null.
+	 * @param message The message to log.
+	 * @param error The error to include with the log entry.
+	 * @param format The arguments to use to format the string.
+	 */
+	public void logError(String message, Throwable error, Object... format){
+		log(Level.SEVERE, error, message, format);
+	}
+	
+	/**
+	 * Logs a notable warning, including a stack trace if error is not null.
+	 * @param message The message to log.
+	 * @param error The error to include with the log entry.
+	 * @param format The arguments to use to format the string.
+	 */
+	public void logWarn(String message, Throwable error, Object... format){
+		log(Level.WARNING, error, message, format);
+	}
+	
+	/**
+	 * Logs a notable warning.
+	 * @param message The message to log.
+	 */
+	public void logWarn(String message){
+		logWarn(message, null);
+	}
+	
+	/**
 	 * Sets the debug logging state.
 	 * @param value Whether to log debug messages.
 	 */
@@ -47,8 +101,57 @@ public class LogHelper {
 		_debugLoggingEnabled = value;
 	}
 	
-	public void log(Level severity, Throwable error, String message, String... format){
-		throw new RuntimeException("Not implemented.");
+	/**
+	 * Logs a message with the specified severity, formatting the string with the specified formatters.
+	 * @param severity The severity of the log message.
+	 * @param message The message to log.
+	 * @param format The objects to pass to String.format, if any.
+	 */
+	public void log(Level severity, String message, Object... format){
+		log(severity, null, message, format);
+	}
+	
+	/**
+	 * Logs a message with info severity, formatting the string with the specified formatters.
+	 * @param message The message to log.
+	 * @param format The objects to pass to String.format, if any.
+	 */
+	public void log(String message, Object... format){
+		log(Level.INFO, message, format);
+	}
+	
+	/**
+	 * Logs a message with the specified severity.
+	 * @param severity The severity of the log message.
+	 * @param message The message to log.
+	 */
+	public void log(Level severity, String message){
+		log(severity, message, (Object[])null);
+	}
+	
+	/**
+	 * Logs a message with the specified severity, formatting the string with the specified formatters.
+	 * @param severity The severity of the log message.
+	 * @param error The error to record, or null if none.
+	 * @param message The message to log.
+	 * @param format The objects to use to format the string, if any.
+	 */
+	public void log(Level severity, Throwable error, String message, Object... format){
+		if(severity == null){
+			throw new IllegalArgumentException("The severity must not be null.");
+		}
+		
+		if(message == null){
+			throw new IllegalArgumentException("The message must not be null.");
+		}
+		
+		message = processLogStatement(message, format);
+		
+		if(error == null){
+			_logged.getLogger().log(severity, message);
+		}else{
+			_logged.getLogger().log(severity, message, error);
+		}
 	}
 	
 	/**
@@ -75,13 +178,7 @@ public class LogHelper {
 			return;
 		}
 		
-		if(format != null && format.length > 0){
-			message = String.format(message, format);
-		}
-		
-		if(isColorStrippingEnabled()){
-			message = ChatColor.stripColor(message);
-		}
+		message = processLogStatement(message, format);
 		
 		if(error == null){
 			_logged.getLogger().log(Level.INFO, message);
