64b0172028606386f48dc45d366c78b9ecf14946
Glen Husman <wappokickingdeer@gmail.com>
2014-04-07 19:38:36 -0700

    Serialize yaw and pitch in location class

--- src/me/pagekite/glen3b/library/bukkit/datastore/SerializableLocation.java
+++ src/me/pagekite/glen3b/library/bukkit/datastore/SerializableLocation.java
@@ -1,6 +1,5 @@
 package me.pagekite.glen3b.library.bukkit.datastore;
 
-import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,10 +13,11 @@ import org.bukkit.configuration.serialization.ConfigurationSerializable;
  * Represents a YAML-serializable location class.
  * @author Glen Husman
  */
-public final class SerializableLocation implements Serializable, ConfigurationSerializable {
-	private static final long serialVersionUID = 4330155630564355916L;
+public final class SerializableLocation implements ConfigurationSerializable {
+
 	private double x,y,z;
-    private String world;
+	private float yaw,pitch;
+	private String world;
     
     /**
      * Creates a SerializableLocation from a location.
@@ -28,31 +28,46 @@ public final class SerializableLocation implements Serializable, ConfigurationSe
     	x=loc.getX();
         y=loc.getY();
         z=loc.getZ();
+        yaw = loc.getYaw();
+        pitch = loc.getPitch();
         world=loc.getWorld().getName();
     }
     
     /**
      * Gets the deserialized location.
-     * @return The Location instance, or null if the serialized form was invalid.
+     * @return A new Location instance representing this serialized state, or {@code null} if the serialized form was invalid.
      */
     public Location getLocation() {
         World w = Bukkit.getWorld(world);
         if(w==null)
             return null;
         Location toRet = new Location(w,x,y,z);
+        toRet.setPitch(pitch);
+        toRet.setYaw(yaw);
         return toRet;
     }
     
     /**
-     * Deserialization constructor.
+     * Internally used deserialization constructor. Should not be called by client code.
      * @see ConfigurationSerializable
      * @param serialized The serialized form of the object.
      */
   	public SerializableLocation(Map<String, Object> serialized){
+  		Validate.notNull(serialized, "The object map is null.");
   		x = (Double)serialized.get("x");
   		y = (Double)serialized.get("y");
   		z = (Double)serialized.get("z");
   		world = (String)serialized.get("world");
+  		
+  		// Backwards compatibility with client code
+  		// This is a MUST because server owners may have config files which may have old versions of this serialized class
+  		// If that is the case, we must still be able to deserialize them
+  		// Hence, we need to check for new keys and assume defaults if they don't exist
+  		if(serialized.containsKey("yaw"))
+  			yaw = (Float)serialized.get("yaw");
+  		
+  		if(serialized.containsKey("pitch"))
+  			pitch = (Float)serialized.get("pitch");
   	}
     
   	/**
@@ -66,6 +81,8 @@ public final class SerializableLocation implements Serializable, ConfigurationSe
 		objects.put("y", y);
 		objects.put("z", z);
 		objects.put("world", world);
+		objects.put("pitch", pitch);
+		objects.put("yaw", yaw);
 		return objects;
 	}
 }
