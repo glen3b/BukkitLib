7e6a317e66922afd547353080abcf62db4c011e2
Glen Husman <wappokickingdeer@gmail.com>
2014-04-18 23:30:18 -0700

    Added invokeMethod reflection util This method SEARCHES method overloads for acceptable methods to call It has significant overhead, but does not require the types of the arguments to be passed to the method from the caller

--- src/me/pagekite/glen3b/library/bukkit/ReflectionUtilities.java
+++ src/me/pagekite/glen3b/library/bukkit/ReflectionUtilities.java
@@ -3,10 +3,16 @@ package me.pagekite.glen3b.library.bukkit;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.List;
 
 import org.apache.commons.lang.Validate;
 import org.bukkit.Bukkit;
 import org.bukkit.Server;
+
+import com.google.common.base.Function;
+import com.google.common.collect.Lists;
 
 /**
  * Utilities involving reflection.
@@ -38,6 +44,94 @@ public final class ReflectionUtilities {
 	}
 
 	/**
+	 * Invoke a method on an object. This will iterate through all methods of the object to determine the appropriate overload.
+	 * <p>If a specific overload is desired, the following may be used instead to avoid the overhead of this method.
+	 * <pre>
+	 * {@code
+	 * Method method = object.getClass().getMethod(method, argumentTypes);
+	 * method.setAccessible(true);
+	 * method.invoke(object, varArgs);
+	 * }
+	 * </pre>
+	 * </p>
+	 * 
+	 * <p>
+	 * Variable arguments methods must be invoked with a parameter of an array, not as a varargs invocation.
+	 * That array must also be cast to {@code Object} so the varargs invocation of this method does not interpret that array as the argument array passed to this method.
+	 * </p>
+	 * @param object The object upon which to invoke the method.
+	 * @param method The name of the method to invoke.
+	 * @param args The arguments to pass to the method.
+	 * @return The result of the invoked method.
+	 * @author Glen Husman
+	 * @see Class#getMethod(String, Class...)
+	 * @see Method#setAccessible(boolean)
+	 * @see Method#invoke(Object, Object...)
+	 */
+	public static Object invokeMethod(Object object, String method, Object... args) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+		Validate.notNull(object, "The object instance must not be null.");
+		Validate.notEmpty(method, "The method name must be defined.");
+
+		List<Method> possibleMethods = Lists.newArrayListWithExpectedSize(2); // Assume 2 method overloads as a default, but the list will resize
+
+		for(Method m : object.getClass().getMethods()){
+			if(!m.getName().equals(method)){
+				continue;
+			}
+			
+			if(m.isVarArgs()){
+				possibleMethods.add(m);
+			}else if(m.getGenericParameterTypes().length == args.length){
+				Type[] argTypes = m.getGenericParameterTypes();
+				boolean isAcceptableOverload = true;
+				for(int i = 0; i < args.length; i++){
+					if(argTypes[i] instanceof Class){
+						isAcceptableOverload = ((Class<?>)argTypes[i]).isAssignableFrom(args[i].getClass());
+					}else{
+						// TODO: Support more types
+						isAcceptableOverload = false;
+					}
+
+					if(!isAcceptableOverload){
+						break;
+					}
+				}
+
+				if(isAcceptableOverload){
+					possibleMethods.add(m);
+				}
+			}
+		}
+
+		for (Method m : possibleMethods){
+			try{
+				return m.invoke(object, args);
+			}catch(IllegalArgumentException err){
+				// Assume bad method overload, continue
+			}
+		}
+
+		// No method found that would properly invoke
+		throw new NoSuchMethodException("Could not find an appropriate method overload with the specified name. Methods found by loose match: " +
+				Arrays.toString(new Function<List<Method>, String[]>(){
+
+					@Override
+					public String[] apply(List<Method> paramF) {
+						String[] value = new String[paramF.size()];
+						for(int i = 0; i < paramF.size(); i++){
+							value[i] = paramF.get(i).toGenericString();
+						}
+						return value;
+					}
+
+				}.apply(possibleMethods)));
+
+		//Method handleMethod = object.getClass().getMethod(method);
+		//handleMethod.setAccessible(true);
+		//return handleMethod.invoke(object, args);
+	}
+
+	/**
 	 * Gets the NMS handle for the specified bukkit object (NOT the CraftBukkit class). Should be compatible across versions as it uses reflection.
 	 * @param entity The bukkit object instance for which to retrieve the handle using the {@code getHandle()} method.
 	 * @return The NMS handle for the specified CraftBukkit object.
@@ -49,13 +143,13 @@ public final class ReflectionUtilities {
 	 * @throws IllegalAccessException If the method cannot be successfully accessed.
 	 */
 	public static Object getNMSHandle(Object entity) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-		Validate.notNull(entity, "The entity must not be null.");
-		
+		Validate.notNull(entity, "The object instance must not be null.");
+
 		Method handleMethod = entity.getClass().getMethod("getHandle");
 		handleMethod.setAccessible(true);
 		return handleMethod.invoke(entity);
 	}
-	
+
 	/**
 	 * Gets the component of the package name of NMS and OBC classes which represents the minecraft server version.
 	 * Determines this value by using the fully qualified class and package name of the CraftBukkit implementation class of {@link Server}.
