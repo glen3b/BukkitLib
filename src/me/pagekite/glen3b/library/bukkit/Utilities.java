b720b6f6198f3bc52f242d8f8419880cb2a6d579
Glen Husman <wappokickingdeer@gmail.com>
2014-04-25 16:45:23 -0700

    Added clamp method

--- src/me/pagekite/glen3b/library/bukkit/Utilities.java
+++ src/me/pagekite/glen3b/library/bukkit/Utilities.java
@@ -60,21 +60,66 @@ public final class Utilities {
 	 * @author Glen Husman
 	 */
 	static final class EventRegistrar implements Listener{
-		
+		// TODO Use it!
+	}
+
+	/**
+	 * Assure that a value is between two other values.
+	 * @param value The value to clamp.
+	 * @param minimum The minimum return value.
+	 * @param maximum The maximum return value.
+	 * @return {@code value} if it is between {@code minimum} and {@code maximum}, {@code minimum} if {@code value < minimum}, or {@code maximum} of {@code value > maximum}.
+	 */
+	public static float clamp(float value, float minimum, float maximum) {
+		return Math.max(Math.min(value, maximum), minimum);
 	}
 	
+	/**
+	 * Assure that a value is between two other values.
+	 * @param value The value to clamp.
+	 * @param minimum The minimum return value.
+	 * @param maximum The maximum return value.
+	 * @return {@code value} if it is between {@code minimum} and {@code maximum}, {@code minimum} if {@code value < minimum}, or {@code maximum} of {@code value > maximum}.
+	 */
+	public static double clamp(double value, double minimum, double maximum) {
+		return Math.max(Math.min(value, maximum), minimum);
+	}
+	
+	/**
+	 * Assure that a value is between two other values.
+	 * @param value The value to clamp.
+	 * @param minimum The minimum return value.
+	 * @param maximum The maximum return value.
+	 * @return {@code value} if it is between {@code minimum} and {@code maximum}, {@code minimum} if {@code value < minimum}, or {@code maximum} of {@code value > maximum}.
+	 */
+	public static long clamp(long value, long minimum, long maximum) {
+		return Math.max(Math.min(value, maximum), minimum);
+	}
+	
+	/**
+	 * Assure that a value is between two other values.
+	 * @param value The value to clamp.
+	 * @param minimum The minimum return value.
+	 * @param maximum The maximum return value.
+	 * @return {@code value} if it is between {@code minimum} and {@code maximum}, {@code minimum} if {@code value < minimum}, or {@code maximum} of {@code value > maximum}.
+	 */
+	public static int clamp(int value, int minimum, int maximum) {
+		return Math.max(Math.min(value, maximum), minimum);
+	}
+
+
 	/**
 	 * @deprecated Use {@link Constants#TICKS_PER_SECOND}
 	 */
 	@Deprecated
 	public static final long TICKS_PER_SECOND = Constants.TICKS_PER_SECOND;
-	
+
 	/**
 	 * @deprecated Use {@link Constants#TICKS_PER_MINUTE}
 	 */
 	@Deprecated
 	public static final long TICKS_PER_MINUTE = Constants.TICKS_PER_MINUTE;
-	
+
 	/**
 	 * Utility methods involving entities.
 	 * @author Glen Husman
@@ -83,7 +128,7 @@ public final class Utilities {
 		private Entities(){
 			//No instance should be created
 		}
-		
+
 		/**
 		 * Gets the entity with the specified UUID of the specified type. This method will search the specified world and will return the first entity with the specified UUID.
 		 * @param <T> The type of the entity.
@@ -97,7 +142,7 @@ public final class Utilities {
 			Validate.notNull(id, "The ID cannot be null.");
 			Validate.notNull(world, "The world cannot be null.");
 			Validate.notNull(clazz, "The entity class must not be null.");
-			
+
 			if(Player.class.isAssignableFrom(clazz)){
 				for(Player e : world.getPlayers()){
 					if(e != null && e.isValid() && e.getUniqueId().equals(id)){
@@ -105,16 +150,16 @@ public final class Utilities {
 					}
 				}
 			}else{
-			for(T e : world.getEntitiesByClass(clazz)){
-				if(e != null && e.isValid() && e.getUniqueId().equals(id)){
-					return e;
+				for(T e : world.getEntitiesByClass(clazz)){
+					if(e != null && e.isValid() && e.getUniqueId().equals(id)){
+						return e;
+					}
 				}
 			}
-			}
-			
+
 			return null;
 		}
-		
+
 		/**
 		 * Gets the entity with the specified UUID of the specified type. This method will search all worlds registered with the Bukkit API, and will return the first entity with the specified UUID.
 		 * @param <T> The type of the entity.
@@ -128,10 +173,10 @@ public final class Utilities {
 					return entity;
 				}
 			}
-			
+
 			return null;
 		}
-		
+
 		/**
 		 * Gets the entity with the specified UUID. This method will search the specified world and will return the first entity with the specified UUID.
 		 * @param id The persistent universally unique identifier of the entity.
@@ -141,16 +186,16 @@ public final class Utilities {
 		public static Entity getEntity(UUID id, World world){
 			Validate.notNull(id, "The ID cannot be null.");
 			Validate.notNull(world, "The world cannot be null.");
-			
+
 			for(Entity e : world.getEntities()){
 				if(e != null && e.isValid() && e.getUniqueId().equals(id)){
 					return e;
 				}
 			}
-			
+
 			return null;
 		}
-		
+
 		/**
 		 * Gets the entity with the specified UUID. This method will search all worlds registered with the Bukkit API, and will return the first entity with the specified UUID.
 		 * @param id The persistent universally unique identifier of the entity.
@@ -163,11 +208,11 @@ public final class Utilities {
 					return entity;
 				}
 			}
-			
+
 			return null;
 		}
 	}
-	
+
 	/**
 	 * Utility methods involving players.
 	 * @author Glen Husman
@@ -176,7 +221,7 @@ public final class Utilities {
 		private Players(){
 			//No instance should be created
 		}
-		
+
 		/**
 		 * Gets a list of the {@link UUID}s of all currently online players.
 		 * <p>
@@ -197,14 +242,14 @@ public final class Utilities {
 		 */
 		public static List<UUID> getOnlinePlayerIDs(){
 			ArrayList<UUID> players = new ArrayList<UUID>();
-			
+
 			for(Player player : Bukkit.getServer().getOnlinePlayers()){
 				players.add(player.getUniqueId());
 			}
-			
+
 			return players;
 		}
-		
+
 		/**
 		 * Gets a list of the names of the specified players. Keep in mind that names are no longer safe as persistent cross-session unique identifiers.
 		 * @param players The {@code Player}s for which to retrieve the names.
@@ -213,14 +258,14 @@ public final class Utilities {
 		 */
 		public static List<String> getPlayerNames(Player... players){
 			ArrayList<String> playerNames = new ArrayList<String>();
-			
+
 			for(Player player : players){
 				playerNames.add(player.getName());
 			}
-			
+
 			return playerNames;
 		}
-		
+
 		/**
 		 * Gets a list of the names of all currently online players. Keep in mind that names are no longer safe as persistent cross-session unique identifiers.
 		 * @return A mutable list of all of the usernames of all of the players currently online on the {@code Bukkit} server.
@@ -231,7 +276,7 @@ public final class Utilities {
 			return getPlayerNames(Bukkit.getOnlinePlayers());
 		}
 	}
-	
+
 	/**
 	 * Utility methods involving execution scheduling.
 	 * @author Glen Husman
@@ -240,7 +285,7 @@ public final class Utilities {
 		private Scheduler(){
 			//No instance should be created
 		}
-		
+
 		/**
 		 * Schedules a task to execute every second, starting one second after the call to this method.
 		 * @param host The plugin under which to schedule this task. This parameter may not be {@code null}.
@@ -256,7 +301,7 @@ public final class Utilities {
 
 			return async ? Bukkit.getScheduler().runTaskTimer(host, task, Constants.TICKS_PER_SECOND, Constants.TICKS_PER_SECOND) : Bukkit.getScheduler().runTaskTimerAsynchronously(host, task, Constants.TICKS_PER_SECOND, Constants.TICKS_PER_SECOND);
 		}
-		
+
 		/**
 		 * Schedules a task to execute after one tick.
 		 * @param host The plugin under which to schedule this task. This parameter may not be {@code null}.
@@ -272,7 +317,7 @@ public final class Utilities {
 
 			return async ? Bukkit.getScheduler().runTaskLater(host, task, 1L) : Bukkit.getScheduler().runTaskLaterAsynchronously(host, task, 1L);
 		}
-		
+
 		/**
 		 * Schedules a task to execute on the main server thread after one tick.
 		 * @param host The plugin under which to schedule this task. This parameter may not be {@code null}.
@@ -283,7 +328,7 @@ public final class Utilities {
 		public static int scheduleTickTask(Plugin host, Runnable task){
 			return scheduleTickTask(host, task, false).getTaskId();
 		}
-		
+
 		/**
 		 * Run the specified tasks after the completion of the specified teleport. This method is intended to wrap calls to {@link TeleportationManager} methods which may return a {@code null} {@link QueuedTeleport}. If the method returns {@code null} and that value is passed into this method, the tasks will run instantly after the teleport, as was intended, without an additional {@code null} check in client code.
 		 * @param <T> The type of the destination of the teleport.
@@ -294,26 +339,26 @@ public final class Utilities {
 		 */
 		public static <T> boolean runAfterTeleport(QueuedTeleport<T> teleport, Runnable... tasks){
 			Validate.noNullElements(tasks, "There must not be any null tasks.");
-			
+
 			if(teleport == null || teleport.isCancelled()){
 				// Instant execution
 				for(Runnable task : tasks){
 					task.run();
 				}
-				
+
 				return false;
 			}else{
 				// Queue execution
 				for(Runnable task : tasks){
 					teleport.registerOnTeleport(task);
 				}
-				
+
 				return true;
 			}
 		}
-		
+
 	}
-	
+
 	/**
 	 * @deprecated Use {@link Scheduler#runAfterTeleport(QueuedTeleport, Runnable...)}
 	 */
@@ -321,17 +366,17 @@ public final class Utilities {
 	public static <T> boolean runAfterTeleport(QueuedTeleport<T> teleport, Runnable... tasks){
 		return Scheduler.runAfterTeleport(teleport, tasks);
 	}
-	
+
 	/**
 	 * Utility methods involving arguments, argument parsing, and value parsing.
 	 * @author Glen Husman
 	 */
 	public static final class Arguments{
-	
+
 		private Arguments(){
 			//No instance should be created
 		}
-		
+
 		/**
 		 * Attempt to parse {@code str} as a double=precision real number, returning a default value if it is not possible.
 		 * @param str The string to attempt to parse.
@@ -342,14 +387,14 @@ public final class Utilities {
 			if(str == null || str.trim().isEmpty()){
 				return defaultVal;
 			}
-			
+
 			try{
 				return Double.parseDouble(str);
 			}catch(NumberFormatException thr){
 				return defaultVal;
 			}
-	    }
-		
+		}
+
 		/**
 		 * Attempt to parse {@code str} as an integer, returning a default value if it is not possible.
 		 * @param str The string to attempt to parse.
@@ -360,23 +405,23 @@ public final class Utilities {
 			if(str == null || str.trim().isEmpty()){
 				return defaultVal;
 			}
-			
+
 			try{
 				return Integer.parseInt(str);
 			}catch(NumberFormatException thr){
 				return defaultVal;
 			}
-	    }
+		}
 	}
-	
+
 	/**
 	 * @deprecated Use {@link Arguments#parseInt(String, int)}
 	 */
 	@Deprecated
 	public static int parseInt(String str, int defaultVal){
 		return Arguments.parseInt(str, defaultVal);
-    }
-	
+	}
+
 	/**
 	 * Schedules a task to execute after one tick.
 	 * @param host The plugin under which to schedule this task. If this parameter is {@code null}, the GBukkitLib plugin instance as retrieved by the {@code PluginManager} will be used for scheduling. <b>Using this method with a {@code null} plugin argument is deprecated, and this functionality will be removed in a future release.</b>
@@ -390,12 +435,12 @@ public final class Utilities {
 	@Deprecated
 	public static BukkitTask scheduleTickTask(Plugin host, Runnable task, boolean async){
 		Validate.notNull(task, "The task must not be null.");
-		
+
 		Plugin hostPl = host == null ? Bukkit.getServer().getPluginManager().getPlugin("GBukkitLib") : host;
-		
+
 		return async ? Bukkit.getScheduler().runTaskLater(hostPl, task, 1L) : Bukkit.getScheduler().runTaskLaterAsynchronously(hostPl, task, 1L);
 	}
-	
+
 	/**
 	 * Schedules a task to execute on the main server thread after one tick.
 	 * @param host The plugin under which to schedule this task. If this parameter is {@code null}, the GBukkitLib plugin instance as retrieved by the {@code PluginManager} will be used for scheduling. <b>Using this method with a {@code null} plugin argument is deprecated, and this functionality will be removed in a future release.</b>
@@ -408,21 +453,21 @@ public final class Utilities {
 	public static int scheduleTickTask(Plugin host, Runnable task){
 		return scheduleTickTask(host, task, false).getTaskId();
 	}
-	
+
 	/**
 	 * Utility methods involving items.
 	 * @author Glen Husman
 	 */
 	public static final class Items{
 		private Items(){}
-		
+
 		/**
 		 * Utility methods involving potions and potion effects.
 		 * @author Glen Husman
 		 */
 		public static final class Potions{
 			private Potions(){}
-			
+
 			private static final List<PotionEffectType> _negativeEffects = Arrays.asList(
 					PotionEffectType.BLINDNESS,
 					PotionEffectType.CONFUSION,
@@ -433,8 +478,8 @@ public final class Utilities {
 					PotionEffectType.SLOW_DIGGING,
 					PotionEffectType.WEAKNESS,
 					PotionEffectType.WITHER
-				);
-			
+					);
+
 			/**
 			 * Determines if a potion effect is a negative effect.
 			 * @param effect The effect to check.
@@ -442,11 +487,11 @@ public final class Utilities {
 			 */
 			public static boolean isNegative(PotionEffectType effect){
 				Validate.notNull(effect, "The potion effect is null.");
-				
+
 				return _negativeEffects.contains(effect);
 			}
 		}
-		
+
 		/**
 		 * Sets the display name of the specified item. <b>It also removes any existing lore.</b>
 		 * The item that is passed will be unmodified after the operation.
@@ -457,7 +502,7 @@ public final class Utilities {
 		public static ItemStack setItemName(ItemStack item, String name) {
 			return setItemNameAndLore(item, name, null);
 		}
-		
+
 		/**
 		 * Enchants the specified item, ignoring restrictions on level.
 		 * The item that is passed will be unmodified after the operation.
@@ -477,7 +522,7 @@ public final class Utilities {
 			nItem.setItemMeta(im);
 			return nItem;
 		}
-		
+
 		/**
 		 * Sets the color of leather armor. The item that is passed will be unmodified after the operation.
 		 * @param item The item to modify the data of. If it does not have the appropriate metadata for the operation, this will throw an exception.
@@ -495,7 +540,7 @@ public final class Utilities {
 			nItem.setItemMeta(im);
 			return nItem;
 		}
-		
+
 		/**
 		 * Sets the display name and lore of the specified item. The item that is passed will be unmodified after the operation.
 		 * @param item The item to modify the data of.
@@ -512,7 +557,7 @@ public final class Utilities {
 			if(lore != null){
 				Validate.noNullElements(lore, "The lore array contains null elements.");
 			}
-			
+
 			ItemMeta im = item.getItemMeta();
 			im.setDisplayName(name);
 			im.setLore(lore == null ? emptyList : Arrays.asList(lore));
@@ -521,7 +566,7 @@ public final class Utilities {
 			return nItem;
 		}
 	}
-	
+
 	/**
 	 * @deprecated Use {@link Items#setItemName(ItemStack, String)}, it has a less misleading name.
 	 */
@@ -529,7 +574,7 @@ public final class Utilities {
 	public static ItemStack setItemName(ItemStack item, String name) {
 		return Items.setItemName(item, name);
 	}
-	
+
 	/**
 	 * Sets the display name of the specified item. It also removes any lore.
 	 * @param item The item to modify the data of.
@@ -541,10 +586,10 @@ public final class Utilities {
 	public static ItemStack setItemNameAndLore(ItemStack item, String name) {
 		return setItemNameAndLore(item, name, new String[0]);
 	}
-	
+
 	// TODO: Is this safe? Is there a better way? It's immutable, is that bad?
 	private static List<String> emptyList = Collections.emptyList();
-	
+
 	/**
 	 * @deprecated Use {@link Items#setItemNameAndLore(ItemStack, String, String[])}.
 	 */
@@ -553,9 +598,9 @@ public final class Utilities {
 			String[] lore) {
 		return Items.setItemNameAndLore(item, name, lore);
 	}
-	
+
 	private Utilities(){
 		//No instance should be created
 	}
-	
+
 }
