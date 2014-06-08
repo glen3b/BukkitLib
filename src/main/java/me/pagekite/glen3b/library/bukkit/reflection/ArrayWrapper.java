4842f9756f32df34d64778c90be5c8f1f894d94e
Glen Husman <wappokickingdeer@gmail.com>
2014-06-07 23:16:25 -0700

    Added toArray(Iterable, Class) method to array wrapper. This allows ANY iterable to be copied to an array, assuming it has a constant iteration order and size. The order of iterated elements will be used for the order of elements in the array.

--- src/main/java/me/pagekite/glen3b/library/bukkit/reflection/ArrayWrapper.java
+++ src/main/java/me/pagekite/glen3b/library/bukkit/reflection/ArrayWrapper.java
@@ -1,7 +1,9 @@
 package me.pagekite.glen3b.library.bukkit.reflection;
 
 import java.io.Serializable;
+import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Collection;
 
 import org.apache.commons.lang.Validate;
 
@@ -85,5 +87,38 @@ public final class ArrayWrapper<E> implements Cloneable, Serializable {
     @Deprecated
 	public final ArrayWrapper<E> clone() throws CloneNotSupportedException{
 		return new ArrayWrapper<E>(_array.clone());
+    }
+	
+	/**
+     * Converts an iterable element collection to an array of elements.
+     * The iteration order of the specified object will be used as the array element order.
+     * @param list The iterable of objects which will be converted to an array.
+     * @param c The type of the elements of the array.
+     * @return An array of elements in the specified iterable.
+     */
+     @SuppressWarnings("unchecked")
+    public static <T> T[] toArray(Iterable<? extends T> list, Class<T> c) {
+        int size = -1;
+        if(list instanceof Collection<?>){
+        	@SuppressWarnings("rawtypes")
+			Collection coll = (Collection)list;
+        	size = coll.size();
+        }
+        
+        
+        if(size < 0){
+        	size = 0;
+        	// Ugly hack: Count it ourselves
+        	for(@SuppressWarnings("unused") T element : list){
+        		size++;
+        	}
+        }
+    	
+        T[] result = (T[]) Array.newInstance(c, size);
+        int i = 0;
+        for(T element : list){ // Assumes iteration order is consistent
+    		result[i++] = element; // Assign array element at index THEN increment counter
+    	}
+        return result;
     }
 }
