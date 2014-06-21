c6dbc9dfcdf25d42d19d140d7a244bd3be6fc545
Glen Husman <wappokickingdeer@gmail.com>
2014-06-21 15:39:35 -0700

    Fix package name initialization error

--- src/main/java/me/pagekite/glen3b/library/bukkit/reflection/InternalPackage.java
+++ src/main/java/me/pagekite/glen3b/library/bukkit/reflection/InternalPackage.java
@@ -24,55 +24,60 @@ public enum InternalPackage implements PackageClassSource{
 	/**
 	 * Represents the {@code org.bukkit.craftbukkit} package, also known as OBC. This package contains CraftBukkit implementation classes of Bukkit interfaces and abstract classes.
 	 */
-	CRAFTBUKKIT(Bukkit.getServer().getClass().getPackage().getName());
-	
+	CRAFTBUKKIT(null);
+
 	private String _package;
-	
+
+	private InternalPackage(){
+		this(null);
+	}
+
 	private InternalPackage(String pkg){
 		loadedClasses = Maps.newHashMap();
 		_cachedClassView = Collections.unmodifiableCollection(loadedClasses.values());
 	}
-	
+
 	void initPackageName(){
 		if(_package != null){
 			return;
 		}
-		
+
 		switch(this){
 		case MINECRAFT_SERVER:
 			_package =  "net.minecraft.server." + ReflectionUtilities.getPackageVersionString();
 			break;
-			default:
-				break;
+		case CRAFTBUKKIT:
+			_package = Bukkit.getServer().getClass().getPackage().getName();
+			break;
+		default:
+			break;
 		}
 	}
-	
+
 	public String getPackage(){
-		if(_package == null){
-			initPackageName();
-		}
-		
+		initPackageName();
+
 		return _package;
 	}
-	
+
 	@Override
 	public String toString(){
 		return getPackage();
 	}
-	
+
 	@Override
 	public synchronized Class<?> getClass(String className) throws ClassNotFoundException {
 		Validate.notEmpty(className, "The class name must be specified.");
-		
+
 		String cName = className.trim();
 		String fqcName = getPackage() + ClassUtils.PACKAGE_SEPARATOR + cName;
-		
+
 		Class<?> retVal = null;
 		Exception errCause = null;
-		
+
 		if(loadedClasses.containsKey(cName)){
 			retVal = loadedClasses.get(cName);
-			
+
 			if(retVal == null){
 				errCause = new NullPointerException("The cached Class instance representing " + fqcName + " is null.");
 			}
@@ -87,22 +92,22 @@ public enum InternalPackage implements PackageClassSource{
 
 			loadedClasses.put(cName, retVal);
 		}
-		
+
 		if(retVal == null){
 			throw new ClassNotFoundException(fqcName + " does not exist.", errCause);
 		}
-		
+
 		return retVal;
 	}
 
 	Map<String, Class<?>> loadedClasses; // Package-private to allow for resetCache to work
 	private Collection<Class<?>> _cachedClassView;
-	
+
 	@Override
 	public Collection<Class<?>> getCachedClasses() {
 		return _cachedClassView;
 	}
-	
+
 	/**
 	 * Represents a sub-package of {@link InternalPackage#CRAFTBUKKIT org.bukkit.craftbukkit} which further divides that package into categories.
 	 * @author Glen Husman
@@ -181,21 +186,24 @@ public enum InternalPackage implements PackageClassSource{
 		 * Represents the subpackage containing utility classes written by and included with CraftBukkit.
 		 */
 		UTILITY("util");
-		
+
 		private String _fullyQualifiedPackage;
 		private String _name;
-		
+
 		public String getPackage(){
+			if(_fullyQualifiedPackage == null){
+				_fullyQualifiedPackage = InternalPackage.CRAFTBUKKIT.getPackage() + ClassUtils.PACKAGE_SEPARATOR + _name;
+			}
+			
 			return _fullyQualifiedPackage;
 		}
-		
+
 		private SubPackage(String name){
 			_name = name;
-			_fullyQualifiedPackage = InternalPackage.CRAFTBUKKIT + "." + name;
 			loadedClasses = Maps.newHashMap();
 			_cachedClassView = Collections.unmodifiableCollection(loadedClasses.values());
 		}
-		
+
 		/**
 		 * Gets the name of this subpackage.
 		 * @return The name of this subpackage.
@@ -203,20 +211,20 @@ public enum InternalPackage implements PackageClassSource{
 		public String getSubpackageName(){
 			return _name;
 		}
-		
+
 		private static Map<String, SubPackage> BY_NAME;
-		
+
 		static {
 			SubPackage[] vals = values();
-			
+
 			BY_NAME = Maps.newHashMapWithExpectedSize(vals.length);
-			
-	        for (SubPackage subpkg : vals) {
-	            
-	            BY_NAME.put(subpkg.getSubpackageName(), subpkg);
-	        }
-	    }
-		
+
+			for (SubPackage subpkg : vals) {
+
+				BY_NAME.put(subpkg.getSubpackageName(), subpkg);
+			}
+		}
+
 		/**
 		 * Gets the subpackage with the specified name. The name is trimmed by this method.
 		 * @param name The name of the subpackage.
@@ -225,25 +233,25 @@ public enum InternalPackage implements PackageClassSource{
 		public static SubPackage getBySubpackageName(String name){
 			return BY_NAME.get(name == null ? null : name.toLowerCase().trim());
 		}
-		
+
 		@Override
 		public String toString(){
-			return _fullyQualifiedPackage;
+			return getPackage();
 		}
-		
+
 		@Override
 		public synchronized Class<?> getClass(String className) throws ClassNotFoundException {
 			Validate.notEmpty(className, "The class name must be specified.");
-			
+
 			String cName = className.trim();
 			String fqcName = getPackage() + ClassUtils.PACKAGE_SEPARATOR + cName;
-			
+
 			Class<?> retVal = null;
 			Exception errCause = null;
-			
+
 			if(loadedClasses.containsKey(cName)){
 				retVal = loadedClasses.get(cName);
-				
+
 				if(retVal == null){
 					errCause = new NullPointerException("The cached Class instance representing " + fqcName + " is null.");
 				}
@@ -258,17 +266,17 @@ public enum InternalPackage implements PackageClassSource{
 
 				loadedClasses.put(cName, retVal);
 			}
-			
+
 			if(retVal == null){
 				throw new ClassNotFoundException(fqcName + " does not exist.", errCause);
 			}
-			
+
 			return retVal;
 		}
 
 		Map<String, Class<?>> loadedClasses; // Package-private to allow for resetCache to work
 		private Collection<Class<?>> _cachedClassView;
-		
+
 		@Override
 		public Collection<Class<?>> getCachedClasses() {
 			return _cachedClassView;
